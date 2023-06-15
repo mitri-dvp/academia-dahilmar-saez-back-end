829d8056fc0d70765cfb270721db9e20020dcb87
mitri-dvp <mitri.dvp@gmail.com>
2023-06-15 12:04:54 -0500

    Schedule delete development.

--- src/api/schedule/controllers/schedule.ts
+++ src/api/schedule/controllers/schedule.ts
@@ -39,4 +39,70 @@ module.exports = {
 
     return { schedules: group.schedules };
   },
+  async update(ctx) {
+    const user = ctx.state.user;
+    const { scheduleID } = ctx.params;
+
+    const {
+      data: { scheduleData },
+    } = ctx.request.body as CreateBody;
+
+    if (user.role.type !== USER_ROLES.TRAINER) {
+      return ctx.badRequest("Rol de usuario no autorizado");
+    }
+
+    await strapi.query("api::schedule.schedule").update({
+      where: { id: scheduleID },
+      data: {
+        datetime: scheduleData.datetime,
+      },
+    });
+
+    const group = await strapi.query("api::group.group").findOne({
+      where: {
+        id: scheduleData.groupID,
+      },
+      populate: {
+        schedules: true,
+      },
+    });
+
+    return { schedules: group.schedules };
+  },
+  async deleteSchedule(ctx) {
+    const user = ctx.state.user;
+    const { scheduleID } = ctx.params;
+
+    if (user.role.type !== USER_ROLES.TRAINER) {
+      return ctx.badRequest("Rol de usuario no autorizado");
+    }
+
+    const schedule = await strapi.query("api::schedule.schedule").findOne({
+      where: { id: scheduleID },
+      populate: {
+        group: {
+          select: ["id"],
+        },
+      },
+    });
+
+    if (!schedule) {
+      return ctx.badRequest("Horario no encontrado");
+    }
+
+    await strapi.query("api::schedule.schedule").delete({
+      where: { id: schedule.id },
+    });
+
+    const group = await strapi.query("api::group.group").findOne({
+      where: {
+        id: schedule.group.id,
+      },
+      populate: {
+        schedules: true,
+      },
+    });
+
+    return { schedules: group.schedules };
+  },
 };
--- src/api/schedule/routes/schedule.ts
+++ src/api/schedule/routes/schedule.ts
@@ -5,5 +5,15 @@ module.exports = {
       path: "/schedules",
       handler: "schedule.create",
     },
+    {
+      method: "PUT",
+      path: "/schedules/:scheduleID",
+      handler: "schedule.update",
+    },
+    {
+      method: "DELETE",
+      path: "/schedules/:scheduleID",
+      handler: "schedule.deleteSchedule",
+    },
   ],
 };
